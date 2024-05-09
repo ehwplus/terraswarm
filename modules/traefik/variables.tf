537d6cefc8522215c42a298b4be998fd155b38a3
Thomas Faust <mail@thomas-hesse.eu>
2024-05-09 23:09:36 +0200

    fix(traefik): remove source from traefik_certificate default

--- modules/traefik/variables.tf
+++ modules/traefik/variables.tf
@@ -351,7 +351,6 @@ variable "traefik_certificate" {
   description = "Configuration settings for a Docker volume used by Traefik to manage SSL certificates. The 'driver' field allows for driver-specific options in a map format. The 'source' field specifies the path or identifier of the volume source. The 'target' field defines the destination path within the container, defaulting to '/etc/certificates'. The 'type' field indicates the mount type, with 'bind' as the default."
   default = {
     driver_name = "local",
-    source      = "/path/to/certificate",
     type        = "bind"
   }
 
